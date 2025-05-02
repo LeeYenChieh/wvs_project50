b39875c29ab1e43b8fb2cc54851630d9acb68c7e
mjlee <yenchieh0826@gmail.com>
2025-05-02 16:02:00 +0800

    uri comment

--- wvs_project50/constants/Uri.ts
+++ wvs_project50/constants/Uri.ts
@@ -3,6 +3,6 @@ const ENDPOINT = 'https://m0xju1.logto.app';
 const API_BASE_URL = `${ENDPOINT}/api`;
 const API_RESOURCE = 'https://api.m0xju1.logto.app/api'
 const AppId = 'k57fns1jagvscm2mx4a3t';
-const BackendURL = 'http://192.168.0.105:4000'
+const BackendURL = 'http://192.168.0.105:4000' // You should use your IP！
 
 export { RedirectUri, ENDPOINT, API_BASE_URL, API_RESOURCE, AppId, BackendURL };
